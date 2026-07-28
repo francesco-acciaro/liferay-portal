3c694c33c77f2f620d18cbd2ab81918b194e8900
Francesco Acciaro <francesco.acciaro@liferay.com>
2026-07-28 11:22:41 +0200

    Add the liferay-learn-etc-cron-importer client extension
    This is the skeleton of the CronJob that will import the liferay-learn documentation: the job definition, the container image and an entry point that does nothing yet beyond proving the wiring.

---
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-etc-cron-importer/src/main/java/com/liferay/learn/Main.java
@@ -0,0 +1,18 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.learn;
+
+/**
+ * @author Francesco Acciaro
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println(
+			"liferay-learn-etc-cron-importer: F0 scaffold, no-op run");
+	}
+
+}
